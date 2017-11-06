767f587313e06bce46e3d66d350186f1b32e3f73
Julianne Kim <juliannne@gmail.com>
2017-11-06 19:57:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,12 +65,14 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT *
+      SELECT COUNT(*)
       FROM students
       WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end
 
   def self.students_below_12th_grade
